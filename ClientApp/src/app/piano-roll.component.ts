571c472493aba3417517488957575311407cd54d
Zakir Gowani <zakirgowani@gmail.com>
2018-08-22 23:47:41 -0500

    Angular mainting piano roll grid state

--- ClientApp/src/app/piano-roll.component.ts
+++ ClientApp/src/app/piano-roll.component.ts
@@ -14,7 +14,50 @@ export class PianoRollComponent implements OnInit {
     // constructor(private service: NoteService) { }
 
     ngOnInit() {
-    this.notes = [] // this.service.getNotes();
+        var timeStateLength = 16;
+        var stateWidth = (100 / timeStateLength) + "%";
+
+        this.notes = [
+            {'color' : 'white', 'note' : 'b', 'octave' : 2},
+            {'color' : 'black', 'note' : 'as', 'octave' : 2},
+            {'color' : 'white', 'note' : 'a', 'octave' : 2},
+            {'color' : 'black', 'note' : 'gs', 'octave' : 2},
+            {'color' : 'white', 'note' : 'g', 'octave' : 2},
+            {'color' : 'black', 'note' : 'fs', 'octave' : 2},
+            {'color' : 'white', 'note' : 'f', 'octave' : 2},
+            {'color' : 'white', 'note' : 'e', 'octave' : 2},
+            {'color' : 'black', 'note' : 'ds', 'octave' : 2},
+            {'color' : 'white', 'note' : 'd', 'octave' : 2},
+            {'color' : 'black', 'note' : 'cs', 'octave' : 2},
+            {'color' : 'white', 'note' : 'c', 'octave' : 2},
+
+            {'color' : 'white', 'note' : 'b', 'octave' : 1},
+            {'color' : 'black', 'note' : 'as', 'octave' : 1},
+            {'color' : 'white', 'note' : 'a', 'octave' : 1},
+            {'color' : 'black', 'note' : 'gs', 'octave' : 1},
+            {'color' : 'white', 'note' : 'g', 'octave' : 1},
+            {'color' : 'black', 'note' : 'fs', 'octave' : 1},
+            {'color' : 'white', 'note' : 'f', 'octave' : 1},
+            {'color' : 'white', 'note' : 'e', 'octave' : 1},
+            {'color' : 'black', 'note' : 'ds', 'octave' : 1},
+            {'color' : 'white', 'note' : 'd', 'octave' : 1},
+            {'color' : 'black', 'note' : 'cs', 'octave' : 1},
+            {'color' : 'white', 'note' : 'c', 'octave' : 1}
+        ]
+
+        this.gridState = [];
+        for (let note of this.notes) {
+            var noteRow = {
+                'color' : note.color,
+                'octave' : note.octave,
+                'note' : note.note,
+                'timeStates' : Array.apply(null, Array(timeStateLength)).map(Number.prototype.valueOf,0),
+                'stateWidth' : stateWidth
+            };
+            this.gridState.push(noteRow);
+        }
+        console.log(this.gridState);
+
     }
 
     addNote(note: Note) { this.selectedNotes.append(note); }
