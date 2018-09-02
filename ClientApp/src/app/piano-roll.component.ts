01f80ec155d94a2d251472c7d210fbba3409c920
Zakir Gowani <zakirgowani@gmail.com>
2018-09-02 13:42:17 -0500

    Relocated piano roll code from the track component

---
+++ ClientApp/src/app/piano-roll.component.ts
@@ -0,0 +1,140 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import { GenerationService } from './generation.service';
+import { ConfigDataService } from './configdata.service';
+
+@Component({
+    selector:    'piano-roll',
+    templateUrl: './piano-roll.html',
+    providers:  [ /*NoteService*/ ],
+    styleUrls: ['./piano-roll.component.css']
+})
+
+export class PianoRollComponent {
+    timeStateLength = 8;
+    gridState = [];
+    conformToKeyScale = true;
+    generationType = 'melody';
+    octaveConstraint = 1;
+    octaveConstraintCheck = true;
+    notes: Array<any>;
+    _ref: any;
+
+    @Input() trackNumber: number;
+    @Input() key: string;
+    @Input() scale: Object;
+    @Input() pianoRollOpen: boolean;
+
+    @Output()
+    noteDrawn = new EventEmitter<any>();
+
+    @Output()
+    trackChange = new EventEmitter<any>();
+
+    constructor(public generationService: GenerationService, public configDataService: ConfigDataService) {
+        this.initializeEmptyGridState();
+    }
+
+    initializeEmptyGridState() {
+        var stateWidth = (100 / this.timeStateLength) + "%";
+
+        this.notes = [
+            {'color' : 'white', 'note' : 'b', 'octave' : 2},
+            {'color' : 'black', 'note' : 'as', 'octave' : 2},
+            {'color' : 'white', 'note' : 'a', 'octave' : 2},
+            {'color' : 'black', 'note' : 'gs', 'octave' : 2},
+            {'color' : 'white', 'note' : 'g', 'octave' : 2},
+            {'color' : 'black', 'note' : 'fs', 'octave' : 2},
+            {'color' : 'white', 'note' : 'f', 'octave' : 2},
+            {'color' : 'white', 'note' : 'e', 'octave' : 2},
+            {'color' : 'black', 'note' : 'ds', 'octave' : 2},
+            {'color' : 'white', 'note' : 'd', 'octave' : 2},
+            {'color' : 'black', 'note' : 'cs', 'octave' : 2},
+            {'color' : 'white', 'note' : 'c', 'octave' : 2},
+
+            {'color' : 'white', 'note' : 'b', 'octave' : 1},
+            {'color' : 'black', 'note' : 'as', 'octave' : 1},
+            {'color' : 'white', 'note' : 'a', 'octave' : 1},
+            {'color' : 'black', 'note' : 'gs', 'octave' : 1},
+            {'color' : 'white', 'note' : 'g', 'octave' : 1},
+            {'color' : 'black', 'note' : 'fs', 'octave' : 1},
+            {'color' : 'white', 'note' : 'f', 'octave' : 1},
+            {'color' : 'white', 'note' : 'e', 'octave' : 1},
+            {'color' : 'black', 'note' : 'ds', 'octave' : 1},
+            {'color' : 'white', 'note' : 'd', 'octave' : 1},
+            {'color' : 'black', 'note' : 'cs', 'octave' : 1},
+            {'color' : 'white', 'note' : 'c', 'octave' : 1}
+        ]
+
+        this.gridState = [];
+        for (let note of this.notes) {
+            var noteRow = {
+                'color' : note.color,
+                'octave' : note.octave,
+                'note' : note.note,
+                'timeStates' : Array.apply(null, Array(this.timeStateLength)).map(Number.prototype.valueOf,0),
+                'stateWidth' : stateWidth,
+                'id' : note.note + note.octave
+            };
+            this.gridState.push(noteRow);
+        }
+    }
+
+    ngOnInit() {
+
+    }
+
+    togglePianoRollOpen() {
+        console.log('hey');
+    }
+
+    clearPianoRoll() {
+        for (var i = 0; i < this.gridState.length; i++) {
+            this.gridState[i]['timeStates'] = Array.apply(null, Array(this.timeStateLength)).map(Number.prototype.valueOf,0);
+        }
+        this.noteDrawn.emit({'event': 'clear'});
+    }
+
+    noteDrawnHandler(noteName, noteOctave, noteState) {
+        this.noteDrawn.emit({ 'event' : 'noteDrawn', 'note' : noteName + noteOctave, 'state' : noteState});
+    }
+
+    destroyReference() {
+        this._ref.destroy();
+    }
+
+    generate(generationOptions) {
+        console.log(generationOptions);
+
+        generationOptions.key = generationOptions.conformToKeyScale ? this.configDataService.key : 'any';
+        generationOptions.scale = generationOptions.conformToKeyScale ? this.configDataService.scale : 'any';
+        generationOptions.octaveConstraint = generationOptions.octaveConstraintCheck ? generationOptions.octaveConstraint : 'any';
+
+
+        var generatedNotes = [];
+        this.generationService.generate(generationOptions, this.timeStateLength).subscribe((data) => {
+            generatedNotes = data['generationResult'];
+            this.renderNotes(generatedNotes);
+            this.noteDrawn.emit({'event': 'generation'});
+        });
+    }
+
+    renderNotes(notesToRender) {
+        this.clearPianoRoll();
+        var timeStateIndex = 0;
+        for (var timeStateIndex = 0; timeStateIndex < notesToRender.length; timeStateIndex++) {
+            this.renderNotesInOneTimeStep(notesToRender[timeStateIndex], timeStateIndex);
+        }
+    }
+
+    renderNotesInOneTimeStep(notesToRenderInThisTimeStep, timeStateIndex) {
+        for (let noteToRender of notesToRenderInThisTimeStep) {
+            for (var noteIndex = 0; noteIndex < this.gridState.length; noteIndex++) {
+                if(this.gridState[noteIndex].note == noteToRender.note && this.gridState[noteIndex].octave == noteToRender.octave) {
+                    this.gridState[noteIndex]['timeStates'][timeStateIndex] = true;
+                    break;
+                }
+            }
+        }
+    }
+}
