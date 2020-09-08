e1ccb0dc93a67bcb44f4dccfea6511d780ff0be9
Zakir Gowani <zakirgowani@gmail.com>
2020-09-07 21:57:11 -0500

    feat: added chord roman numeral naming scheme

--- ServerApp/generation_tools.py
+++ ServerApp/generation_tools.py
@@ -25,16 +25,14 @@ def get_key_scale_notes(key, scale_code, octave, allowed_chromatic_notes):
     index_of_key = allowed_chromatic_notes.index({'note' : key.lower(), 'octave' : octave})
     notes_sorted_starting_at_root = allowed_chromatic_notes[index_of_key:] + allowed_chromatic_notes[:index_of_key]
     notes_sorted_starting_at_root = notes_sorted_starting_at_root * 2 # double list to make note selection circular
-    print(notes_sorted_starting_at_root)
     intervals = constants['scales'][scale_code]['intervals']
-    print(intervals)
     key_scale_notes = []
     i = 0
     for interval in intervals:
-        print(notes_sorted_starting_at_root[i + interval])
         key_scale_notes.append( notes_sorted_starting_at_root[i + interval] )
         i = i + interval
-    return key_scale_notes
+    # Rotate the result so that the tonic is first in the list
+    return key_scale_notes[-1:] + key_scale_notes[:-1]
 
 def generate_random_melody(length, key, scale, octave):
     allowed_notes = get_allowed_notes(key, scale, octave)
@@ -43,7 +41,6 @@ def generate_random_melody(length, key, scale, octave):
     for i in range(length):
         result.append([random.choice(allowed_notes)])
     
-    print('melody:' + str(result))
     return result
     
 def generate_random_chords(length, key, scale, octave):
@@ -65,12 +62,11 @@ def generate_random_chords(length, key, scale, octave):
             unused_chord_tones.pop(generated_note_index)
         result.append(chord)
             
-    print('chords:' + str(result))
     return result
     
-def determine_chord_name(chord):
+def determine_chord_name(chord, key, scale):
     if len(chord) <= 2:
-        return ''
+        return ('', '')
     notes = map(lambda x: x.upper(), chord)
     notes = list(map(lambda x: x.replace('S', '#'), notes))
     notes.reverse()
@@ -89,8 +85,57 @@ def determine_chord_name(chord):
         except Exception as e:
             print(e)
 
-    return abbreviation
+    return (abbreviation, determine_chord_degree(abbreviation, key, scale, c))
+
+def get_degree_for_root(chord_root, sorted_allowed_notes):
+    degree = 1
+    for note in sorted_allowed_notes:
+        if note['note'] == chord_root:
+            break
+        degree += 1
+    return degree
+
+
+def determine_chord_degree(chord_name, key, scale, chord_object):
+    if len(chord_name.strip()) == 0:
+        return ''
+
+    pieces = chord_name.split()
+    chord_root = pieces[0].lower().replace('#', 's')
+    sorted_allowed_notes = get_allowed_notes(key, scale['code'], 0)
     
+    degree = get_degree_for_root(chord_root, sorted_allowed_notes)
+    modifier = '' # sharp or flat
+
+    if degree == 8:
+        upper_neighbor = ''
+        lower_neighbor = ''
+        if len(chord_root) == 1:
+            upper_neighbor = chord_root + 's'
+        else:
+            lower_neighbor = chord_root[0]
+        if len(upper_neighbor) > 0:
+            degree = get_degree_for_root(upper_neighbor, sorted_allowed_notes)
+            modifier = 'b'
+        if len(lower_neighbor) > 0:
+            degree = get_degree_for_root(lower_neighbor, sorted_allowed_notes)
+            modifier = '#'
+
+    quality = chord_object.quality
+    roman_numeral = constants['roman_numerals_upper'][degree - 1]
+
+    if quality == 'major':
+        return roman_numeral + modifier + ' ' + ' '.join(pieces[1:])
+    if quality == 'augmented':
+        return roman_numeral + modifier + '+ ' +  ' '.join(pieces[1:])
+    if quality == 'minor':
+        return roman_numeral.lower() + modifier + ' ' + ' '.join(pieces[1:])
+    if quality == 'diminished':
+        return roman_numeral.lower() + modifier + '\xB0 ' +  ' '.join(pieces[1:])
+    
+    return roman_numeral.lower() + modifier + ' ' + ' '.join(pieces[1:])
+
+
 def transpose_notes_to_grid(notes):
     grid = []
     timeStateLength = 8
@@ -101,10 +146,14 @@ def transpose_notes_to_grid(notes):
                 notes_at_this_step.append(note['note'])
         grid.append(notes_at_this_step)
     return grid
-        
-def name_chords_in_tracks(tracks):
+
+def name_chords_in_tracks(tracks, key, scale):
     grid_by_tracks = list(map(transpose_notes_to_grid, tracks))
-    chord_names_by_tracks = []
+    coupled = []
     for grid in grid_by_tracks:
-        chord_names_by_tracks.append(list(map(lambda x: determine_chord_name(x), grid)))
-    return chord_names_by_tracks
+        coupled.append(list(map(lambda x: determine_chord_name(x, key, scale), grid)))
+    
+    chord_names_by_tracks = [ [x[0] for x in track] for track in coupled ]
+    chord_degrees_by_tracks = [ [x[1] for x in track] for track in coupled ]
+    
+    return chord_names_by_tracks, chord_degrees_by_tracks
